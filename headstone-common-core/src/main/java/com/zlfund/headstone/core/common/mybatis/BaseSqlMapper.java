79f9a38c0c4d5aaf48b82b7616beba808a4a120a
xuwf <xuwf@jjmmw.com>
2017-02-21 15:22:51 +0800

    determine mybatis usage

---
+++ headstone-common-core/src/main/java/com/zlfund/headstone/core/common/mybatis/BaseSqlMapper.java
@@ -0,0 +1,11 @@
+package com.zlfund.headstone.core.common.mybatis;
+
+/**
+ * MyBatis Mapper接口的父接口
+ * @author: 徐文凡 
+ * @since: 2017年2月21日 下午3:20:58 
+ * @history:
+ */
+public interface BaseSqlMapper {
+
+}
---
+++ headstone-service-trade/src/main/java/com/zlfund/headstone/core/mapper/TradeRequestMapper.java
@@ -0,0 +1,32 @@
+/**
+ * @Title TradeRequestMapper.java 
+ * @Package com.zlfund.headstone.core.mapper 
+ * @Description 
+ * @author 徐文凡
+ * @date 2017年2月21日 下午2:22:55 
+ * @version V1.0   
+ */
+package com.zlfund.headstone.core.mapper;
+
+import java.util.List;
+
+import com.zlfund.headstone.core.common.mybatis.BaseSqlMapper;
+import com.zlfund.headstone.core.dao.po.TradeRequestPO;
+
+/** 
+ * @author: 徐文凡 
+ * @since: 2017年2月21日 下午2:22:55 
+ * @history:
+ */
+public interface TradeRequestMapper extends BaseSqlMapper {
+
+    TradeRequestPO getTradeRequest();
+
+    List<TradeRequestPO> listTradeRequest();
+
+    int countTradeRequest();
+
+    int saveTradeRequest(TradeRequestPO tradeRequestPO);
+
+    int updateTradeRequest(TradeRequestPO tradeRequestPO);
+}
